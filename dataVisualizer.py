5b45ae97bac8c1cba8c04335203c01f25ee55728
Pierrick Jay <pierrick.jay1@gmail.com>
2024-01-04 15:43:58 +0100

    adding predict price

--- dataVisualizer.py
+++ dataVisualizer.py
@@ -1,12 +1,25 @@
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
+import sys
+import csv
 
 
 def parseCsv(file_obj):
-	content = file_obj.read()
-	names =  content.split()
-	print(names)
+	km_list = []
+	price_value = []
+	reader = csv.DictReader(file_obj)
+	sorted_rows = sorted(reader, key= lambda row:int(row['km']))
+	km_list = [int(row['km']) for row in sorted_rows]
+	price_value = [int(row['price']) for row in sorted_rows]
+	return (km_list, price_value)
 
-
+km_list = []
+price_value = []
 f = open(sys.argv[1])
-parseCsv(f)
+km_list, price_value= parseCsv(f)
+print("km = ", km_list, "price value =",price_value)
+fig, ax = plt.subplots() # fig is the figure and ax the value x, and y
+ax.plot(km_list,price_value)
+# ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+plt.show()
+
 
