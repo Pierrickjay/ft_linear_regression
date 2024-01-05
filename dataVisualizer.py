70f525206ded646fefb33c9cba506f3838c44534
Pierrick Jay <pierrick.jay1@gmail.com>
2024-01-05 13:43:52 +0100

    improving displaying graph; still issue with t0 & t1

--- dataVisualizer.py
+++ dataVisualizer.py
@@ -2,23 +2,24 @@ import matplotlib.pyplot as plt
 import sys
 import csv
 
-
 def parseCsv(file_obj):
 	km_list = []
 	price_value = []
 	reader = csv.DictReader(file_obj)
-	sorted_rows = sorted(reader, key= lambda row:int(row['km']))
-	km_list = [int(row['km']) for row in sorted_rows]
-	price_value = [int(row['price']) for row in sorted_rows]
+	for row in reader:
+		km_list.append(int(row['km']))
+		price_value.append(int(row['price']))
 	return (km_list, price_value)
 
 km_list = []
 price_value = []
 f = open(sys.argv[1])
 km_list, price_value= parseCsv(f)
-print("km = ", km_list, "price value =",price_value)
-fig, ax = plt.subplots() # fig is the figure and ax the value x, and y
-ax.plot(km_list,price_value)
+plt.scatter(km_list, price_value, color='blue', marker='o', label='Data Points')
+plt.xlabel('Mileage (km)')
+plt.ylabel('Price')
+plt.title('Scatter Plot of Mileage vs. Price')
+plt.legend()
 # ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
 plt.show()
 
